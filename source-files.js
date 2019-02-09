a9fcb4655146fde622d47fcbe4baf9216379a99b
Travis CI User <travis@example.org>
2019-02-09 08:48:44 +0000

    Update documentation

---
+++ source-files.js
@@ -0,0 +1,5 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["lazy_static"] = {"name":"","dirs":[],"files":["lazy.rs","lib.rs"]};
+sourcesIndex["openvr"] = {"name":"","dirs":[{"name":"compositor","dirs":[],"files":["mod.rs","texture.rs"]},{"name":"system","dirs":[],"files":["event.rs","mod.rs"]}],"files":["chaperone.rs","lib.rs","property.rs","render_models.rs","tracking.rs"]};
+sourcesIndex["openvr_sys"] = {"name":"","dirs":[],"files":["bindings.rs","lib.rs"]};
+createSourceSidebar();
